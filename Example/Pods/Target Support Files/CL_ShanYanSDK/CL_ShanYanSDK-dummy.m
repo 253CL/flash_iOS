53f1b2a1524a9fd50ed72da62aa17d6fca165ab3
wanglijun <CAODA19920605w>
2018-12-03 19:08:27 +0800

    no message

--- Example/Pods/Target Support Files/CL_ShanYanSDK/CL_ShanYanSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CL_ShanYanSDK : NSObject
-@end
-@implementation PodsDummy_CL_ShanYanSDK
-@end
--- Example/Pods/Target Support Files/Pods-CL_ShanYanSDK_Tests/Pods-CL_ShanYanSDK_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CL_ShanYanSDK_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_CL_ShanYanSDK_Tests
-@end
