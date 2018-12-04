557f54975c0b5faf4151812c71c4c71fb847d25a
wanglijun <CAODA19920605w>
2018-12-04 09:16:55 +0800

    no message

---
+++ CL_ShanYanSDK/Classes/TYRZSDK.framework/Headers/TYRZUILogin.h
@@ -0,0 +1,76 @@
+//
+//  TYRZUILogin.h
+//  TYRZ
+//
+//  Created by 林涛 on 2017/6/21.
+//  Copyright © 2017年 林涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UACustomModel.h"
+
+@interface TYRZUILogin : NSObject
+
+/**
+ SDK版本号
+ */
+@property (nonatomic,class,readonly) NSString *sdkVersion;
+
+/**
+ 网络类型及运营商
+ 
+ 网络类型  key:networkType    value:0.无网络/ 1.数据流量 / 2.wifi / 3.数据+wifi
+ 运营商   key:carrier         value:0.未知 / 1.中国移动 / 2.中国联通 / 3.中国电信
+ */
+@property (nonatomic,class,readonly) NSDictionary *networkType;
+
+/**
+ 初始化SDK参数
+
+ @param appId 申请能力平台成功后，分配给网站的appId
+ @param appKey 申请能力平台成功后，分配给网站的appKey
+ */
++ (void)initializeWithAppId:(NSString *)appId appKey:(NSString *)appKey;
+
+/**
+ 控制台日志输出控制（默认关闭）
+
+ @param enable 开关参数
+ */
++ (void)printConsoleEnable:(BOOL)enable;
+
+//预取号
++ (void)preGetPhonenumberWithTimeout:(NSTimeInterval)timeout completion:(void(^)(id sender))complete;
+
+/**
+ 自定义UI样式参数
+
+ @param viewModel UI实例Model（UACustomModel.h可查看属性）
+ @param customViews 自定义视图
+ */
++ (void)customUIWithParams:(UACustomModel *)viewModel
+               customViews:(void(^)(UIView *customAreaView))customViews;
+
+/**
+ 自定义短信验证码开关
+
+ @param flag 开关标记
+ */
++ (void)enableCustomSMS:(BOOL)flag;
+
+//显式登录
++ (void)getTokenExpWithController:(UIViewController *)vc
+                          timeout:(NSTimeInterval)timeout
+                         complete:(void (^)(id sender))complete;
+
+//隐式登录
++ (void)getTokenImpWithTimeout:(NSTimeInterval)timeout
+                      complete:(void (^)(id sender))complete;
+
+/**
+ 删除内存中的缓存
+ */
+
++(BOOL)delectScrip;
+
+@end
---
+++ CL_ShanYanSDK/Classes/account_login_sdk_core.framework/Headers/account_login_sdk_core.h
@@ -0,0 +1,19 @@
+//
+//  account_login_sdk_core.h
+//  account_login_sdk_core
+//
+//  Created by zhuof on 2018/3/19.
+//  Copyright © 2018年 xiaowo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for account_login_sdk_core.
+FOUNDATION_EXPORT double account_login_sdk_coreVersionNumber;
+
+//! Project version string for account_login_sdk_core.
+FOUNDATION_EXPORT const unsigned char account_login_sdk_coreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <account_login_sdk_core/PublicHeader.h>
+
+#import <account_login_sdk_core/UniAuthHelper.h>
